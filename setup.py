6bef1b68102645b7033d910801c37c9cfacdc4b0
LaureDsy <laure.dassy@hotmail.com>
2022-03-21 12:42:28 +0100

    SE

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import setuptools
 from setuptools import setup
 
 setup(name='gym_tictactoe_dassy',
       description='Implementation of an OpenAI Gym learning environment of a simple tic tac toe game.',
       version='0.0.1',
       author='Laure Dassy',
+      url="https://github.com/LaureDas/gym_tictactoe_dassy.git",
+      packages=setuptools.find_packages(),
       install_requires=['gym'])
